7437e837b3cda1d7bf5f0ad2b22036d811eb3c1e
Jason Yee <jasonyee1776@gmail.com>
2018-04-11 20:33:28 -0700

    practice using JavaScript alerts and prompts

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var firstnName = prompt("What is your first name");
+
+var lastName = prompt("What is your last name");
+
+var age = prompt("How old are you (numeric value)");
+
+var month = age * 12;
+
+var day = age * 365;
+
+alert("Hello " + firstnName + " " + lastName +", " + "it looks like you are " + age + " years old. That means you are " + month + " months old and " + day + " days old.... You old as hell..." );
